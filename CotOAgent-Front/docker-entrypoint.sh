50dc00a6fb4fb628f9fc0eb82224b75d9987550e
Derpipose <thefluffmaster2000@gmail.com>
2025-11-06 20:27:14 -0700

    Trying to get kube working

--- CotOAgent-Front/docker-entrypoint.sh
+++ CotOAgent-Front/docker-entrypoint.sh
@@ -1,14 +1,11 @@
 #!/bin/sh
-set -e
 
-# Get the DNS server IP from /etc/resolv.conf
-DNS_SERVER=$(grep nameserver /etc/resolv.conf | head -n1 | awk '{print $2}')
-
-# If we got a DNS server, update nginx config with it
-if [ -n "$DNS_SERVER" ]; then
-    echo "Using DNS server: $DNS_SERVER"
-    sed -i "s/resolver valid=10s;/resolver $DNS_SERVER valid=10s;/" /etc/nginx/nginx.conf
-fi
-
-# Start nginx
+echo "=== Container startup debug info ==="
+echo "Resolv.conf:"
+cat /etc/resolv.conf
+echo ""
+echo "Nginx config resolver line:"
+grep "resolver " /etc/nginx/nginx.conf
+echo ""
+echo "Starting nginx..."
 exec nginx -g "daemon off;"
