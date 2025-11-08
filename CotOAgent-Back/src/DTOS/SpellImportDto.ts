653345ebf713800e5c08830399b3b6e3efa6968f
Derpipose <thefluffmaster2000@gmail.com>
2025-11-08 15:05:07 -0700

    Database integration for the front end.

--- CotOAgent-Back/src/DTOS/SpellImportDto.ts
+++ CotOAgent-Back/src/DTOS/SpellImportDto.ts
@@ -2,10 +2,13 @@ import { z } from 'zod';
 
 // Zod schema for validating spell data from the JSON file
 export const SpellImportSchema = z.object({
+  SpellBranch: z.string().optional().default(''),
+  SpellBook: z.string().optional().default(''),
   SpellName: z.string(),
   ManaCost: z.union([z.string(), z.number()]).transform(val => String(val)),
-  HitDie: z.string(),
-  Description: z.string(),
+  HitDie: z.string().optional().default(''),
+  Description: z.string().optional().default(''),
+  BookLevel: z.string().optional().default(''),
 });
 
 // Zod schema for an array of spells
